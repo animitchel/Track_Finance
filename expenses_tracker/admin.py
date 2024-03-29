1c2b9743eb67bcac469d803552281f93e5b8c2e5
Mitchel <animitchel24@gmail.com>
2024-03-29 03:44:29 +0100

    refactor registerview, admin, form_models and implemented logout popup for confirmation with js

--- expenses_tracker/admin.py
+++ expenses_tracker/admin.py
@@ -2,9 +2,30 @@ from django.contrib import admin
 from .models import Transaction, Budget, Profile
 from django.contrib.auth.admin import User
 
+
 # Register your models here.
 
+class TransactionAdmin(admin.ModelAdmin):
+    ordering = ('id',)
+    list_display = ('category', 'amount')
+    search_fields = ('category', 'description')
+    list_filter = ('category',)
 
-admin.site.register(Transaction)
-admin.site.register(Budget)
-admin.site.register(Profile)
+
+class BudgetAdmin(admin.ModelAdmin):
+    ordering = ('id',)
+    list_display = ('category', 'amount')
+    search_fields = ('category', 'description')
+    list_filter = ('category',)
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    # list_display = ('first_name', 'occupation')
+    ordering = ('id',)
+    search_fields = ('first_name',)
+    list_filter = ('first_name', 'last_name', 'occupation', 'city', 'country')
+
+
+admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(Budget, BudgetAdmin)
+admin.site.register(Profile, ProfileAdmin)
